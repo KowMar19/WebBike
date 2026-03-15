d59d8c83682cdf098221469d4dc4be156842c597
KowMar19 <157230750+KowMar19@users.noreply.github.com>
2026-03-15 09:56:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBMfgwlZ-TXQkDQ1P4ZcJs3rmXTLm0n71s",
-  authDomain: "bikeapp-218b6.firebaseapp.com",
-  projectId: "bikeapp-218b6",
-  storageBucket: "bikeapp-218b6.firebasestorage.app",
-  messagingSenderId: "1085315140827",
-  appId: "1:1085315140827:web:a46b446df6da573eb5d0fe"
-};
